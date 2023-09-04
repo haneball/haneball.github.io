adea7d4df03c7e4408db90cf6d35f182654e37c9
haneball <dendro@yeah.net>
2023-09-04 09:56:24 +0800

    add vuepress-plugin-search-pro

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -1,5 +1,6 @@
-import { defineUserConfig } from "vuepress";
+import { Page, defineUserConfig } from "vuepress";
 import theme from "./theme.js";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/",
@@ -9,6 +10,24 @@ export default defineUserConfig({
   description: "haneball Blog",
 
   theme,
+
+  plugins: [
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category as string,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag as string,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ]
 
   // Enable it with pwa
   // shouldPrefetch: false,
