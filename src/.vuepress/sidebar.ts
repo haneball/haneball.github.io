6945aeb6640acfff25ce2f7e7259419aa88d0cd6
haneball <dendro@yeah.net>
2023-09-05 18:10:30 +0800

    modify sidebar

--- src/.vuepress/sidebar.ts
+++ src/.vuepress/sidebar.ts
@@ -1,13 +1,30 @@
 import { sidebar } from "vuepress-theme-hope";
 
 export default sidebar({
-  "/": [
-    "",
+  "/posts/": [
     {
       text: "笔记",
       icon: "book",
-      prefix: "posts/",
-      children: "structure",
-    },
+      children: [
+        {
+          text: "JavaScript",
+          prefix: "js/",
+          collapsible: true,
+          children: "structure"
+        },
+        {
+          text: "CSS",
+          prefix: "css/",
+          collapsible: true,
+          children: "structure"
+        },
+        {
+          text: "Vue",
+          prefix: "vue/",
+          collapsible: true,
+          children: "structure"
+        },
+      ]
+    }
   ],
 });
